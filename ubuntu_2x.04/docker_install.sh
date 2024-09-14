0198dd528b6a4c131809677f55050f502f0650dc
Ian Scrivener <you@example.com>
2024-09-14 16:43:05 +1000

    init

---
+++ ubuntu_2x.04/docker_install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# https://docs.docker.com/engine/install/ubuntu/
+
+# uninstall unofficial packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+
+# To install the latest version of Docker 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+# start up
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+# verify Docker install
+sudo docker run hello-world
+
+
+#Create the docker group.
+sudo groupadd docker
+
+# Add your user to the docker group.
+sudo usermod -aG docker $USER
+
